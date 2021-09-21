49874de6fdc7687fb6c274b60e73c39b1c265581
legalvao1 <legalvao1@gmail.com>
2021-09-21 16:55:14 -0300

    Desafio 1 - Adicionando a rota para criar um usuario

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,14 @@
+const bodyParser = require('body-parser');
 const app = require('./app');
+const routes = require('./routes');
 
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// const apiRoutes = app.Router();
+
+app.post('/users', routes.users.createUser);
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
