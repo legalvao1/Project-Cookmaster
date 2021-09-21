0f721050a2e263e210685d189d31eb748b3f13f4
legalvao1 <legalvao1@gmail.com>
2021-09-21 20:17:19 -0300

    Desafio 2 - rota no server  para usuario fazer login

--- src/api/server.js
+++ src/api/server.js
@@ -7,8 +7,7 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// const apiRoutes = app.Router();
-
 app.post('/users', routes.users.createUser);
+app.post('/login', routes.login);
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
