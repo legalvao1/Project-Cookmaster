f695f965b72a7cef612ca9e5754720bfca463497
legalvao1 <legalvao1@gmail.com>
2021-09-22 09:10:08 -0300

    Desafio 5 - Model para pegar uma receita pelo id

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const mongoConnection = require('./connection');
 
 const create = async ({ name, ingredients, preparation }, userId) => {
@@ -13,7 +14,16 @@ const getRecipes = async () => {
   return recipes;
 };
 
+const getRecipeById = async (id) => {
+  if (!ObjectId.isValid(id)) return false;
+  const db = await mongoConnection.getConnection();
+  const recipe = await db.collection('recipes').findOne({ _id: ObjectId(id) });
+
+  return recipe;
+};
+
 module.exports = {
   create,
   getRecipes,
+  getRecipeById,
 };
