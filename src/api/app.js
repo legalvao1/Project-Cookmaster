92d1bda66c614896fefe06e27372f5021840b643
legalvao1 <legalvao1@gmail.com>
2021-09-22 16:49:04 -0300

    Desafio 11 - Corrigindo as rotas de server para o app

--- src/api/app.js
+++ src/api/app.js
@@ -3,6 +3,39 @@ const path = require('path');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
+const routes = require('./routes');
+
+const storage = multer.diskStorage({
+  destination: async (_req, _file, callback) => {
+    const folderName = './src/uploads';
+        fs.mkdirSync(folderName, { recursive: true });
+        callback(null, folderName);
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${req.params.id}.${file.mimetype.split('/')[1]}`);
+} });
+  
+const upload = multer({ storage });
+
+const validateJWT = require('../middlewares/validateJWT');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/users', routes.users.createUser);
+app.post('/login', routes.login);
+app.post('/recipes', validateJWT, routes.recipes.createRecipe);
+
+app.get('/recipes', routes.recipes.getRecipes);
+app.get('/recipes/:id', routes.recipes.getRecipeById);
+
+app.put('/recipes/:id', validateJWT, routes.recipes.updateRecipe);
+app.put('/recipes/:id/image', validateJWT, upload.single('image'), routes.recipes.addImage);
+
+app.delete('/recipes/:id', validateJWT, routes.recipes.excludeRecipe);
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
--- src/api/server.js
+++ src/api/server.js
@@ -1,38 +1,5 @@
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const fs = require('fs');
 const app = require('./app');
-const routes = require('./routes');
-
-const storage = multer.diskStorage({
-  destination: async (_req, _file, callback) => {
-    const folderName = './src/uploads';
-        fs.mkdirSync(folderName, { recursive: true });
-        callback(null, folderName);
-  },
-  filename: (req, file, callback) => {
-    callback(null, `${req.params.id}.${file.mimetype.split('/')[1]}`);
-} });
-  
-const upload = multer({ storage });
-
-const validateJWT = require('../middlewares/validateJWT');
 
 const PORT = 3000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.post('/users', routes.users.createUser);
-app.post('/login', routes.login);
-app.post('/recipes', validateJWT, routes.recipes.createRecipe);
-
-app.get('/recipes', routes.recipes.getRecipes);
-app.get('/recipes/:id', routes.recipes.getRecipeById);
-
-app.put('/recipes/:id', validateJWT, routes.recipes.updateRecipe);
-app.put('/recipes/:id/image', validateJWT, upload.single('image'), routes.recipes.addImage);
-
-app.delete('/recipes/:id', validateJWT, routes.recipes.excludeRecipe);
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
