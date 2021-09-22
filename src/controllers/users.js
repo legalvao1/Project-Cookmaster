9cb71f280eb1b00171c977531d957c773b8335aa
legalvao1 <legalvao1@gmail.com>
2021-09-22 20:30:56 -0300

    Desafio 12 - Controller para rota de cadastrar um  administrador

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,18 @@
 const services = require('../services/users');
 
 const createUser = async (req, res) => {
-  const user = await services.createUser(req.body);
+  const user = await services.createUser(req.body, req.path);
+  if (user.err) return res.status(user.err.code).json({ message: user.err.message });
+  res.status(201).json({ user });
+};
+
+const createAdminUser = async (req, res) => {
+  const user = await services.createAdminUser(req.body, req.path);
   if (user.err) return res.status(user.err.code).json({ message: user.err.message });
   res.status(201).json({ user });
 };
 
 module.exports = {
   createUser,
+  createAdminUser,
 };
