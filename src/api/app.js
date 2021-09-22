9df3178e6402aeda5db58255447b2b59d518fef4
legalvao1 <legalvao1@gmail.com>
2021-09-22 11:51:49 -0300

    Desafio 9 - Configuraçao do multer e rota para adicionar uma imagem a uma receita cadastrada

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 module.exports = app;
--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,17 @@
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const app = require('./app');
 const routes = require('./routes');
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, callback) => {
+    callback(null, 'uploads');
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${req.params.id}.${file.mimetype.split('/')[1]}`);
+} });
+  
+const upload = multer({ storage });
 
 const validateJWT = require('../middlewares/validateJWT');
 
@@ -17,6 +28,7 @@ app.get('/recipes', routes.recipes.getRecipes);
 app.get('/recipes/:id', routes.recipes.getRecipeById);
 
 app.put('/recipes/:id', validateJWT, routes.recipes.updateRecipe);
+app.put('/recipes/:id/image', validateJWT, upload.single('image'), routes.recipes.addImage);
 
 app.delete('/recipes/:id', validateJWT, routes.recipes.excludeRecipe);
 
