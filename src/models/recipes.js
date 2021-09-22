8e822e55067284a600795420a23a77243e593142
legalvao1 <legalvao1@gmail.com>
2021-09-22 08:59:00 -0300

    Desafio 4 - Model  para pegar a lista de receitas

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -7,6 +7,13 @@ const create = async ({ name, ingredients, preparation }, userId) => {
   return { _id, name, ingredients, preparation, userId };
 };
 
+const getRecipes = async () => {
+  const db = await mongoConnection.getConnection();
+  const recipes = await db.collection('recipes').find().toArray();
+  return recipes;
+};
+
 module.exports = {
   create,
+  getRecipes,
 };
